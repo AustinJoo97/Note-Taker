a66bc1306ba6d0a740a7eb85229cee2e58fe26c1
AustinJoo97 <austinjoo97@gmail.com>
2021-05-02 14:24:44 -0400

    ready for heroku

--- Develop/db/express.js
+++ Develop/db/express.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
