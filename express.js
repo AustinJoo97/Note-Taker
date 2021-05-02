e00b7c751aed0f225d1aa120d79d0a4d6788efd8
AustinJoo97 <austinjoo97@gmail.com>
2021-05-02 15:35:24 -0400

    reorganized file structure for heroku deployment

--- express.js
+++ express.js
@@ -3,29 +3,29 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
 app.get('/api/notes', (req, res) => {
-    let allNotes = fs.readFileSync(path.join(__dirname, './db.json'));
+    let allNotes = fs.readFileSync(path.join(__dirname, './db/db.json'));
 
     res.send(allNotes);
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
 app.post('/api/notes', (req, res) => {
     let newNote = req.body;
 
-    let allNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db.json')));
+    let allNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
 
     // allNotes = JSON.parse(allNotes);
 
@@ -37,21 +37,21 @@ app.post('/api/notes', (req, res) => {
 
     allNotes.push(newNote);
 
-    fs.writeFileSync(path.join(__dirname, './db.json'), JSON.stringify(allNotes));
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(allNotes));
 
     res.send(newNote); 
 })
 
 app.delete('/api/notes/:id', (req, res) => {
     let chosen = Number(req.params.id);
-    let allNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db.json')));
+    let allNotes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
 
     for(let i = 0; i < allNotes.length; i++){
         if(allNotes[i].id === chosen){
             allNotes.splice(i, 1);
         }
     }
-    fs.writeFileSync(path.join(__dirname, './db.json'), JSON.stringify(allNotes));
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(allNotes));
 
     res.send();
 })
